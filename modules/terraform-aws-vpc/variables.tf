614d65d0631f40d026b00f833d1fa89437ec26e8
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2026-02-09 12:17:41 -0500

    feat: Remove unused aws_region variable from VPC module

--- modules/terraform-aws-vpc/variables.tf
+++ modules/terraform-aws-vpc/variables.tf
@@ -9,12 +9,6 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
