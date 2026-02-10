f7912d25de6e9be0d056e7355b2936e5b5354a76
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2026-02-10 12:36:54 -0500

    feat: Rename aws_instance resource and ensure subnet_id variable is included

--- modules/terraform-aws-compute/main.tf
+++ modules/terraform-aws-compute/main.tf
@@ -1,6 +1,7 @@
 
 
-resource "aws_instance" "example" {
+resource "aws_instance" "this" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  subnet_id     = var.subnet_id
 }
--- modules/terraform-aws-compute/variable.tf
+++ modules/terraform-aws-compute/variable.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "The type of instance to use"
   type        = string
 }
+
+variable "subnet_id" {
+  description = "The ID of the subnet to launch the instance in"
+  type        = string
+}
